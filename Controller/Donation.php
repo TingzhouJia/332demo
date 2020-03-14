13de09ed7711fdbcab7387a8c0790a8be4df89b3
TingzhouJia <joe.tzjia@gmail.com>
2020-03-14 01:21:19 -0400

    donor page update

--- Controller/Donation.php
+++ Controller/Donation.php
@@ -1,6 +1,6 @@
 <?php
 
-include "../model/pdoMysql.class.php"; 
+require_once "../model/pdoMysql.class.php"; 
 
 $typeDonation="typeOfTransaction = 'Donation'";
 
@@ -16,7 +16,7 @@ function findDonorYear($year){
 //get donation for donor
 function findDonor($name){
     global $MyPDO;
-    return  $MyPDO::find("MoneyTransaction","payer= '".$name."' and ".$GLOBALS['typeDonation']);
+    return  $MyPDO::find("MoneyTransaction","payer like '%".$name."%' and ".$GLOBALS['typeDonation']);
        
 }
 //get donor for diff years
@@ -30,5 +30,12 @@ function findAllDonor($year="all"){
 }
 //get namelist of payer
 $donorList=$MyPDO::find("MoneyTransaction",null,array(" DISTINCT payer"));
+
+function totalYearOne($org,$year=2020){
+    global $MyPDO;
+    return $MyPDO::find("MoneyTransaction","YEAR(dateOfTransaction) = ".$year." and payee = '".$org."'",
+    array("COUNT(amount) as count","SUM(amount) as amount"));
+}
+
 
 
